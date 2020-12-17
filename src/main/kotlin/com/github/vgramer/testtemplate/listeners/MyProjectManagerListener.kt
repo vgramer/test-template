d1789a0575c43e6aa6ef04597a3a4809fb43c707
GitHub Action <action@github.com>
2020-12-17 17:58:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/vgramer/testtemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/vgramer/testtemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vgramer.testtemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vgramer.testtemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/vgramer/testtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vgramer/testtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vgramer.testtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vgramer.testtemplate.MyBundle
 
 class MyApplicationService {
 
